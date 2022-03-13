4d210967f68fc1a50f94e779b2c17fdd7f3606c8
yhunlu <yhunlu87@gmail.com>
2022-03-13 05:18:29 +0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const a: number = 5;
-const b: number = 2;
+const a: string = "welcome";
+const b: string = "typescript";
 
 app.get('/', (req:any ,res:any ) => {
-  res.send(`1 + 2 = ${a + b}`);
+  res.send(`${a} to ${b}`);
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
